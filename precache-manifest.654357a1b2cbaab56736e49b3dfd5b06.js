167265d9e8962ef147c20ed714e10ce8d9872053
pyong <muanda1993@hotmail.com>
2020-02-18 23:53:25 +0000

    Updates

---
+++ precache-manifest.654357a1b2cbaab56736e49b3dfd5b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9319288fa984fbd32b3ffcec90adbca4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "80d14281344077bc5195",
+    "url": "/my-app/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "43ba05841f40f8e87113",
+    "url": "/my-app/static/css/main.65aac25d.chunk.css"
+  },
+  {
+    "revision": "80d14281344077bc5195",
+    "url": "/my-app/static/js/2.90d8adb5.chunk.js"
+  },
+  {
+    "revision": "f9908732ca44e9bbcff23810a27da213",
+    "url": "/my-app/static/js/2.90d8adb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43ba05841f40f8e87113",
+    "url": "/my-app/static/js/main.0b1ea72f.chunk.js"
+  },
+  {
+    "revision": "02fe11db3da99a7ce03c",
+    "url": "/my-app/static/js/runtime-main.9d986c7a.js"
+  }
+]);
